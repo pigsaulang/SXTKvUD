2e3d89c68caba66784defa6f354e57fa786825b3
pigsaulang <96730625+pigsaulang@users.noreply.github.com>
2022-03-07 11:06:18 +0700

    update commit

--- 7-3-2002-TH16.py
+++ 7-3-2002-TH16.py
@@ -11,9 +11,11 @@ print(z_score)
 from scipy import stats
 p_one_way_value = 1 - stats.norm.cdf(z_score)
 p_value = p_one_way_value * 2
+print("THỰC HÀNH 16")
 print(p_value)
+
 ## Kiểm tra p-value với mức ý nghĩa là 0.05, như định nghĩa ở trên. P_value < muc_y_nghia=0.05 (P_value ≈ . 1)
 ## Từ đó có thể suy ra p-value của hai đuôi (0.04) nhỏ hơn mức ý nghĩa thống kê là 0.05.
 ## Vậy, bác bỏ giả thuyết không và chấp nhận giả thuyết nghịch.
 ## Kết luận: chứng cứ rằng "Tỷ lệ người dân ở Hà Nội đi làm bằng xe
-## đạp khác với 6%“.ác bỏ giã thuyết "Tỷ lệ người lớn ở Hà Nội đi làm việc bằng xe đạp là 6%"
+## đạp khác với 6%“.ác bỏ giã thuyết "Tỷ lệ người lớn ở Hà Nội đi làm việc bằng xe đạp là 6%" 
--- 7-3-2022-TH15.py
+++ 7-3-2022-TH15.py
@@ -11,6 +11,7 @@ z_score = (f - p) / std_dev
 from scipy import stats
 p_one_way_value = 1 - stats.norm.cdf(z_score)
 p_value = p_one_way_value * 2 
+print("THỰC HÀNH 15")
 print(p_value)
 ##Ta có p-value gần bằng 0.01. Nó có ý nghĩa là giả sử 6% người lớn ở Hà Nội đi làm bằng xe đạp, khả năng ta lấy được một mẫu ngẫu nhiên có 10% người Hà Nội đi làm bằng xe đạp là 1%.
 ##Kiểm tra p-value với mức ý nghĩa là 0.05, như định nghĩa ở trên. P_value < muc_y_nghia=0.05 (P_value ≈ . 1)
